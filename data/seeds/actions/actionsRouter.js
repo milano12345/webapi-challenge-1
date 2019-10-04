eb453a453c032fdca82cc35834ff9989e0c45ac8
milano12345 <dmilano857@gmail.com>
2019-10-04 16:56:06 -0500

    actions router 50% complete

--- data/seeds/actions/actionsRouter.js
+++ data/seeds/actions/actionsRouter.js
@@ -1,21 +1,62 @@
 const express = require("express");
-const dataBase = require("../../dbConfig");
+const dataBase = require("./actionModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  dataBase.get().then(projects => {
-    res.status(200).send(projects);
-  });
+  dataBase
+    .get()
+    .then(projects => {
+      res.status(200).send(projects);
+    })
+    .catch(err => {
+      res.send(err);
+    });
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", validatePostId, (req, res) => {
+  dataBase
+    .get(req.params.id)
+    .then(projects => {
+      res.status(200).send(projects);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 router.delete("/:id", (req, res) => {});
 
 router.put("/:id", (req, res) => {});
 
+router.post("/", (req, res) => {
+  const newAction = req.body;
+  if (!newAction.project_id || !newAction.description || !newAction.notes) {
+    res.status(400).json({
+      errorMessage:
+        "Please provide project_id, notes, and description for the action."
+    });
+  } else {
+    dataBase
+      .insert(newAction)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.json({ message: "error saving the project" });
+      });
+  }
+});
+
 // custom middleware
 
-function validatePostId(req, res, next) {}
+function validatePostId(req, res, next) {
+  dataBase.get(req.params.id).then(project => {
+    if (!project) {
+      res.status(404).send({ message: "No project with the specified ID" });
+    } else {
+      next();
+    }
+  });
+}
 
 module.exports = router;
